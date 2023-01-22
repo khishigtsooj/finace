8aa88903dbc301f953081d7ef7b2df6e565a785a
khishigtsooj <Khishigtsoojbathuyg@gmail.com>
2023-01-22 15:20:46 +0800

    Finace module hiij ehelsen Expense Income objectuudiig uusgene

--- app.js
+++ app.js
@@ -2,11 +2,49 @@
 
 //Дэлгэцтэй ажиллах контроллер
 var  uiController = (function(){
-            
+
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription:".add__description",
+        inputValue:".add__value",
+        addBtn: ".add__btn"
+    };
+      return {
+        getInput:function(){
+           return {
+            type: document.querySelector(DOMstrings.inputType).value,
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+           };
+        },
+        getDOMstrings:function(){
+            return DOMstrings;
+        }
+      };   
 })();
 
 //Санхүүтэй ажиллах контроллер
 var  financeController = (function(){
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var data = {
+        allItems:{
+            inc:[],
+            exp:[]
+        },
+        totals:{
+            inc:0,
+            exp:0
+        }
+    }
             
 })();
 
@@ -14,9 +52,11 @@ var  financeController = (function(){
 
 var  appController = (function(uiController,fnController){
 
+
+
     var ctrlAddItem = function(){
     //1 . Оруулах өгөгдлийг дэлгэцээс олж авна.
-
+console.log(uiController.getInput());
 // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална 
 
 //3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана 
@@ -26,16 +66,27 @@ var  appController = (function(uiController,fnController){
 //5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     };
 
-document.querySelector('.add__btn').addEventListener('click',function(){
- ctrlAddItem();
+var setupEventListeners = function(){
 
-});
-document.addEventListener("keypress",function(event){
-    if(event.keyCode ===13|| event.which === 13)
-    {
+    var DOM =uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener('click',function(){
         ctrlAddItem();
+       
+       });
+       document.addEventListener("keypress",function(event){
+           if(event === 13|| event === 13)
+           {
+               ctrlAddItem();
+           }
+       
+       });
+};
+return {
+    init:function(){
+        setupEventListeners();
     }
-
-});
+}
             
 })(uiController,financeController);
+
+appController.init();
